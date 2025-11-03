5f101935a452a6389b40b4e653bba654bfcb368a
Savitha1125 <savithasengottaiyan7@gmail.com>
2025-11-03 17:35:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import Header from './Header';              
+import VendorForm from './Vendor';
+import Franchise from './Franchise';
+import DeliveryLocation from './Location';
+import SearchBar from './SearchBar';
+import Wishlist from './Wishlist';         
+
+export default function App() {
+  const location = useLocation();
+  const hideHeaderRoutes = ['/vendor', '/franchise'];
+  const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
+
+  return (
+    <>
+      {!shouldHideHeader && (
+        <>
+          <SearchBar />
+          <DeliveryLocation />
+        </>
+      )}
+
+      <Routes>
+        <Route path="/" element={<Header />} />
+        <Route path="/vendor" element={<VendorForm />} />
+        <Route path="/franchise" element={<Franchise />} />
+        <Route path="/wishlist" element={<Wishlist />} /> {/* ✅ */}
+      </Routes>
+    </>
+  );
+}
